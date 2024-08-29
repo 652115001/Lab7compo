b8feb86f40900559a50e6152bc89a5a8ad309a87
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-08-29 21:49:39 +0700

    3.2

--- 331-backend/src/main/java/se331/lab/rest/controller/EventController.java
+++ 331-backend/src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,7 +1,10 @@
 package se331.lab.rest.controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab.rest.entity.Event;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,7 +17,21 @@ import java.util.List;
 public class EventController {
 
     List<Event> eventList;
-
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
+        Event output = null;
+        for (Event event : eventList) {
+            if (event.getId().equals(id)) {
+                output = event;
+                break;
+            }
+        }
+        if (output != null) {
+            return ResponseEntity.ok(output);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+        }
+    }
     @GetMapping("events")
     public ResponseEntity<?> getEventLists(
             @RequestParam(value = "_limit", required = false) Integer perPage,
