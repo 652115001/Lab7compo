dccbb54507cc96917cb81625bfb994e4cc7de9ee
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-08-29 22:20:49 +0700

    5.3

--- 331-backend/src/main/java/se331/lab/rest/service/EventServiceImpl.java
+++ 331-backend/src/main/java/se331/lab/rest/service/EventServiceImpl.java
@@ -1,4 +1,27 @@
 package se331.lab.rest.service;
 
-public class EventServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import se331.lab.rest.dao.EventDao;
+import se331.lab.rest.entity.Event;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl implements EventService {
+    final EventDao eventDao;
+    @Override
+    public Integer getEventSize(){
+        return eventDao.getEventSize();
+    }
+    @Override
+    public List<Event> getEvents(Integer pageSize, Integer page) {
+        return eventDao.getEvents(pageSize, page);
+    }
+
+    @Override
+    public Event getEventById(Long id) {
+        return eventDao.getEventById(id);
+    }
 }
